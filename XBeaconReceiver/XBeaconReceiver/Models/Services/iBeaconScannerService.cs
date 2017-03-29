063126cd029e964bbd018329ad5424cfa073cbab
Takahito Iwasa <iwasa-t@pise.co.jp>
2017-03-29 17:07:26 +0900

    up

--- XBeaconReceiver/XBeaconReceiver/Models/Services/iBeaconScannerService.cs
+++ XBeaconReceiver/XBeaconReceiver/Models/Services/iBeaconScannerService.cs
@@ -13,28 +13,34 @@ namespace XBeaconReceiver.Models.Services
         public static iBeaconScannerService Instance { get; private set; } = new iBeaconScannerService();
         private iBeaconScannerService()
         {
-            Plugin.BluetoothLE.CrossBleAdapter.Current.Scan().Subscribe(scanResult =>
+            Plugin.BluetoothLE.CrossBleAdapter.Current.WhenStatusChanged().Subscribe(status =>
             {
-                if(scanResult.AdvertisementData.ManufacturerData.IsiBeaconAdvertisingPacket())
+                if (status != Plugin.BluetoothLE.AdapterStatus.PoweredOn)
+                    return;
+
+                Plugin.BluetoothLE.CrossBleAdapter.Current.Scan().Subscribe(scanResult =>
                 {
-                    var iBeaconPacket = scanResult.AdvertisementData.ManufacturerData.ToiBeaconAdvertisingPacket();
-                    var existPacket = ScanningPackets.FirstOrDefault(item =>
+                    if (scanResult.AdvertisementData.ManufacturerData.IsiBeaconAdvertisingPacket())
                     {
-                        return item.Uuid == iBeaconPacket.Uuid &&
-                               item.Major == iBeaconPacket.Major &&
-                               item.Minor == iBeaconPacket.Minor;
-                    });
-                    if(existPacket == null)
-                    {
-                        iBeaconPacket.TxPower = scanResult.AdvertisementData.TxPower;
-                        ScanningPackets.Add(iBeaconPacket);
+                        var iBeaconPacket = scanResult.AdvertisementData.ManufacturerData.ToiBeaconAdvertisingPacket();
+                        var existPacket = ScanningPackets.FirstOrDefault(item =>
+                        {
+                            return item.Uuid == iBeaconPacket.Uuid &&
+                                   item.Major == iBeaconPacket.Major &&
+                                   item.Minor == iBeaconPacket.Minor;
+                        });
+                        if (existPacket == null)
+                        {
+                            iBeaconPacket.TxPower = scanResult.AdvertisementData.TxPower;
+                            ScanningPackets.Add(iBeaconPacket);
+                        }
+                        else
+                        {
+                            existPacket.TxPower = scanResult.AdvertisementData.TxPower;
+                            existPacket.LastScanDateTime = iBeaconPacket.LastScanDateTime;
+                        }
                     }
-                    else
-                    {
-                        existPacket.TxPower = scanResult.AdvertisementData.TxPower;
-                        existPacket.LastScanDateTime = iBeaconPacket.LastScanDateTime;
-                    }
-                }
+                });
             });
         }
         
